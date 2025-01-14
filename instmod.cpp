b0170bae0d066375395cc019359d3f8e28f795a8
Goomba 3770 <156492456+hanhanfakedos@users.noreply.github.com>
2025-01-14 20:29:26 +0800

    Create instmod.cpp

---
+++ instmod.cpp
@@ -0,0 +1,14 @@
+// install modules for requiments.txt
+#include <cstdlib>
+#include <iostream>
+
+int main(){
+  std::string pipCommand = "pip install -r requirements.txt";
+  x=system(pipCommand.c_str());
+  if (x != 0){
+    std::cout << "Python is not installed" << std::endl
+  }
+  else{
+    std::cout << "Packages is installed" << std::endl
+  }
+}
